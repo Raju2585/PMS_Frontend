dca4aa9db32d83f80720f66739619d8adbcfb1d7
ShruthiBurka <ShruthiBurka@DESKTOP-SIUQACR>
2024-10-07 13:45:04 +0530

    vital

--- src/components/vitalsigns/VitalSigns.js
+++ src/components/vitalsigns/VitalSigns.js
@@ -1,58 +1,92 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import '../Medicalhistory/VitalSigns.css'; 
-import heartrate from '../Assests/heartrate.jpg';
-import oxygen from '../Assests/oxygen.jpg';
-import bp from '../Assests/bp.jpg';
-import temperature from '../Assests/temperature.jpg';
-import respiratory from '../Assests/respiratory.jpg';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 
-const VitalSigns = () => {
-  const location = useLocation();
-  const vitals = location.state?.vitals || JSON.parse(localStorage.getItem("vitalsigns"));
-  
-  if (!vitals) {
-    return <p>No vital signs data available.</p>;
-  }
+import ecgsignal from './ecgsignal.jpg';
+import VitalImage from './VitalImage.jpg';
+import VitalSignBanner from './vitalsignbanner.jpg';
+import respiration from './respiration.png';
+import bp from './bp.webp';
+import o2 from './o2.avif';
+import temp from './temp.webp';
 
-  // Vital sign data structure
-  const vitalSignData = [
-    { type: 'Heart Rate', value: vitals.heartRate || 'N/A', image: heartrate },
-    { type: 'Oxygen Saturation', value: vitals.oxygenSaturation || 'N/A', image: oxygen },
-    { type: 'Blood Pressure', value: vitals.bloodPressure || 'N/A', image: bp },
-    { type: 'Temperature', value: vitals.temperature || 'N/A', image: temperature },
-    { type: 'Respiratory Rate', value: vitals.respiratoryRate || 'N/A', image: respiratory },
-  ];
+import './VitalSigns.css';
 
-  return (
-    <div className="vital-signs-container mb-4" style={{ width: '60%', margin: '0 auto' }}>
-      <h2 className="vital-signs-title mb-4">Vital Signs</h2>
-      {vitalSignData.length > 0 ? (
-        <div className="row">
-          {vitalSignData.map((sign, index) => (
-            <div className="col-md-4 mb-4" key={index}>
-              <div className="card vital-sign-card">
-                <img 
-                  src={sign.image} 
-                  alt={sign.type} 
-                  className="card-img-top" 
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{sign.type}</h5>
-                  <p className="card-text">
-                    <strong>{sign.value}</strong>
-                  </p>
+function VitalSigns() {
+    const location = useLocation();
+    const vitals = location.state?.vitals || JSON.parse(localStorage.getItem("vitalsigns"));
+    console.log(vitals);
+    
+    if (!vitals) { 
+        return <p>No vital signs data available.</p>; 
+    } 
+
+    const renderVital = (label, value, image,icon) => (
+        <div className="card custom-vital  bg-transparent" style={{width:'500px'}}>
+            <div className='icon d-flex'>
+                <div className="icon-shape">
+                  {icon}
+                   
                 </div>
-              </div>
+                <div className="icon-text  bg-transparent">
+                    <p className="card-text icon-textment">{label}</p>
+                    <p className="icon-value">{typeof value === 'string' || typeof value === 'number' ? value : 'n/a'}</p>
+                </div>
             </div>
-          ))}
+            <div className="card-body bg-transparent">
+                <img src={image} className="card-img-top vital-image img-fluid" alt={label} />
+            </div>
         </div>
-      ) : (
-        <p>No vital signs available.</p>
-      )}
-    </div>
-  );
-};
+    );
+
+    return (
+      <div className='container mr-5 vit d-flex justify-content-between align-items-center' style={{marginRight:'20%'}}>
+        <div className='vital-container' >
+            <div className='vital'>
+                <div className='overview-conditions'>
+                    {/* <div>
+                        <h2>Overview Conditions</h2>
+                        <img src={VitalSignBanner} alt="previe-image" className='img-fluid vital-banner' /> 
+                    </div> */}
+                </div>
+                <div className='my-vitals'>
+                    <div className='vital-heading'>
+                        {/* <h2>My Vital Signs</h2> */}
+                        {/* <div className='vital-condtions d-flex justify-content-around'>
+                            {renderVital('Heart Rate', vitals.heartRate, ecgsignal, <i className="fa-regular fa-heart"></i>)}
+                            {renderVital('Respiratory Rate', vitals.respiratoryRate, respiration,<i class="fa-solid fa-droplet"></i>)}
+                            {renderVital('Blood Pressure', vitals.bloodPressure, bp,<i class="fa-solid fa-droplet"></i>)}
+                            {renderVital('Oxygen Saturation', vitals.oxygenSaturation, o2,<i class="fa-solid fa-droplet"></i>)}
+                            {renderVital('Temperature', vitals.temperature, temp,<i class="fa-solid fa-temperature-three-quarters"></i>)} 
+                        </div> */}
+                         <div className='vital-layout container'>
+                          
+                <div className='single-card'>
+                    {/* <div className="vital-image">
+                            <img src={VitalImage} className='img-fluid vitalImage' alt='vitalimage'/>
+                    </div> */}
+                    <h2>My Heart Condition</h2>
+                          <div className="heart-rate-display">
+                            <div className='hear'>{renderVital('Heart Rate', vitals.heartRate, ecgsignal,<i className="fa-regular fa-heart"></i>)}</div>
+                          </div>
+                </div>
+                <div className='multiple-cards'>
+                <h2>Overview Conditions</h2>
+                <div className='row' >
+                        {renderVital('Respiratory Rate', vitals.respiratoryRate, respiration,<i class="fa-solid fa-droplet"></i> )}
+                        {renderVital('Blood Pressure', vitals.bloodPressure, bp,<i class="fa-solid fa-droplet"></i>)}
+                    </div>
+                    <div className='row'>
+                        {renderVital('Oxygen Saturation', vitals.oxygenSaturation, o2,<i class="fa-solid fa-droplet"></i>)}
+                        {renderVital('Temperature', vitals.temperature, temp,<i class="fa-solid fa-temperature-three-quarters"></i>)}
+                    </div>
+                </div>
+            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
 
 export default VitalSigns;
