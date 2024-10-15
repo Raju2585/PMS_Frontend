771fdbcaead67c282b129ff3f36162e069b7a11e
PavaniMaddu <PavaniMaddu@DESKTOP-6TARQ1J>
2024-10-15 16:37:52 +0530

    doctors,specialist

--- src/components/Doctors/Specailist.js
+++ src/components/Doctors/Specailist.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import api from '../../apiHandler/api';
 import { Link, useLocation } from 'react-router-dom';
 import StarRatings from 'react-star-ratings';
-import './Doctor.css';
+import './Specailist.css'
 
 function Doctor() {
     const [doctors, setDoctors] = useState([]);
@@ -36,22 +36,23 @@ function Doctor() {
     const getRandomRating = () => Math.floor(Math.random() * 5) + 1;
 
     return (
-        <div className='container' style={{ margin:"100px",marginLeft:"300px" }}>
+        <div className='full-page-wrapper'>
+        <div className='container spel-container' style={{ margin:"100px",marginLeft:"300px" }}>
             {loading && <div>Loading...</div>}
             {error && <div>Error: {error.message}</div>}
             {!loading && doctors.length === 0 && <div>No doctors available.</div>}
             {doctors.map(doctor => {
                 const rating = getRandomRating();
                 return (
-                    <div className="Doctors d-flex justify-content-between" key={doctor.doctorId}>
+                    <div className="Specialist-Doctors d-flex justify-content-between" key={doctor.doctorId}>
                         <div className="child Doctor-image">
                             <img
                                 src={`data:image/jpeg;base64,${doctor.image ? doctor.image : ''}`}
-                                className="img-fluid doctor-image"
+                                className="img-fluid spel-doctor-image"
                                 alt={doctor.doctorName}
                             />
                         </div>
-                        <div className="child Doctor-Details">
+                        <div className="child Specialist-Doctor-Details">
                             <h4>{doctor.doctorName}</h4>
                             <p>Specialization: {doctor.specialization}</p>
                             <p>Consultation Fee: Rs.{doctor.consultationFee.toFixed(2)}</p>
@@ -72,6 +73,7 @@ function Doctor() {
                     </div>
                 );
             })}
+        </div>
         </div>
     );
 }
